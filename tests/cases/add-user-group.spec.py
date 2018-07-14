00280d747e119e8b6c7daa41c3dabe485ea0f16c
Rickkwa <rickyk.162@gmail.com>
2018-07-14 14:22:10 -0400

    Tests

--- tests/cases/add-user-group.spec.py
+++ tests/cases/add-user-group.spec.py
@@ -8,19 +8,30 @@ class Test(unittest.TestCase):
         self.b = testinfra.get_backend("ansible://containers")
 
     def test_user_exists_and_uid(self):
-        pass
+        user1 = self.host.user("foo")
+        user2 = self.host.user("bar")
+        assert user1.exists
+        assert user2.exists
+        assert user1.uid == 10001
+        assert user2.uid == 10002
 
     def test_group_exists_and_gid(self):
-        pass
+        group = self.host.group("customgroup")
+        assert group.exists
+        assert group.gid == 10001
 
-    def test_user_in_group(self):
-        pass
+    def test_user1_in_group(self):
+        user = self.host.user("foo")
+        assert "customgroup" in user.groups
 
-    def test_user_not_in_group(self):
-        pass
+    def test_user2_not_in_group(self):
+        user = self.host.user("bar")
+        assert "customgroup" not in user.groups
 
     def test_group_sudoers_file(self):
-        pass
+        file = self.host.file("/etc/sudoers.d/rel_gid_10001")
+        assert file.exists and file.is_file
+        assert file.mode == 0o440
 
 
 if __name__ == "__main__":
--- tests/cases/delete-user-group.spec.py
+++ tests/cases/delete-user-group.spec.py
@@ -6,13 +6,16 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.b = testinfra.get_backend("ansible://containers")
-        # Create user and group to be removed
 
     def test_user_not_exist(self):
-        pass
+        user1 = self.host.user("foo")
+        user2 = self.host.user("bar")
+        assert not user1.exists
+        assert not user2.exists
 
     def test_group_not_exist(self):
-        pass
+        group = self.host.group("customgroup")
+        assert not group.exists
 
 
 if __name__ == "__main__":
